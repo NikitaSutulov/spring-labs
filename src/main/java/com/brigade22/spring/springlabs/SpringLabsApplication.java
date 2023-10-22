ce315811565e1826a2ecae382937e5e2e75bba91
Nikita Sutulov <notsid212@gmail.com>
2023-10-22 21:32:17 +0300

    initial commit
    Added a Spring project skeleton with Spring Boot Dev Tools, Spring Web and Thymeleaf modules.

---
+++ src/main/java/com/brigade22/spring/springlabs/SpringLabsApplication.java
@@ -0,0 +1,13 @@
+package com.brigade22.spring.springlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brigade22/spring/springlabs/SpringLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigade22.spring.springlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
