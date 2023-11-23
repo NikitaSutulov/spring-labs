d4f9746859422cd399a59749f10051fd45d76b05
Nikita Sutulov <notsid212@gmail.com>
2023-11-23 12:26:21 +0200

    tried migration from Thymeleaf to JSP
    Tried migration from Thymeleaf to JSP, have issues with JSTL. Hope this will be fixed in the future.

--- src/main/java/com/brigade22/spring/springlabs/SpringLabsApplication.java
+++ src/main/java/com/brigade22/spring/springlabs/SpringLabsApplication.java
@@ -2,9 +2,16 @@ package com.brigade22.spring.springlabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringLabsApplication {
+public class SpringLabsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringLabsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLabsApplication.class, args);
