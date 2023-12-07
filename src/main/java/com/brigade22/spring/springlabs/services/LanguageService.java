9d0be1c7022f6b9e354dcb944d7aa9bf5cea0b52
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2023-12-08 01:17:36 +0200

    moved logic method to service

--- src/main/java/com/brigade22/spring/springlabs/services/LanguageService.java
+++ src/main/java/com/brigade22/spring/springlabs/services/LanguageService.java
@@ -1,11 +1,14 @@
 package com.brigade22.spring.springlabs.services;
 
+import com.brigade22.spring.springlabs.controllers.requests.DictionaryRequest;
 import com.brigade22.spring.springlabs.entities.Language;
 import com.brigade22.spring.springlabs.repositories.LanguageRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class LanguageService {
@@ -46,6 +49,42 @@ public class LanguageService {
         }
 
         return languageRepository.save(language);
+    }
+
+    public void checkIfLanguageExists(DictionaryRequest requestDictionary) {
+        Language possibleLanguage1 = findByName(requestDictionary.getLanguage1().getName());
+
+        if (possibleLanguage1 != null && !Objects.equals(possibleLanguage1.getCode(), requestDictionary.getLanguage1().getCode())) {
+            throw new ResponseStatusException(
+                    org.springframework.http.HttpStatus.BAD_REQUEST,
+                    "Language code is wrong"
+            );
+        }
+
+        Language possibleLanguage2 = findByName(requestDictionary.getLanguage2().getName());
+
+        if (possibleLanguage2 != null && !Objects.equals(possibleLanguage2.getCode(), requestDictionary.getLanguage2().getCode())) {
+            throw new ResponseStatusException(
+                    org.springframework.http.HttpStatus.BAD_REQUEST,
+                    "Language code is wrong"
+            );
+        }
+
+        if (findByCode(requestDictionary.getLanguage1().getCode()) == null) {
+            saveLanguage(requestDictionary.getLanguage1());
+        }
+        Language language1 = findByCode(requestDictionary.getLanguage1().getCode());
+        if (findByCode(requestDictionary.getLanguage2().getCode()) == null) {
+            saveLanguage(requestDictionary.getLanguage2());
+        }
+        Language language2 = findByCode(requestDictionary.getLanguage2().getCode());
+
+        if (!Objects.equals(requestDictionary.getLanguage1().getName(), language1.getName()) || !Objects.equals(requestDictionary.getLanguage2().getName(), language2.getName())) {
+            throw new ResponseStatusException(
+                    org.springframework.http.HttpStatus.BAD_REQUEST,
+                    "Language name is wrong"
+            );
+        }
     }
 
     @PostConstruct
