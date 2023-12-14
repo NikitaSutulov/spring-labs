b3dd20ad9ec9440a355441b87e0426a9779dab3b
Oleksandr <sa44ko@gmail.com>
2023-12-14 22:36:18 +0200

    Refactored Exception to return 404.

--- src/main/java/com/brigade22/spring/springlabs/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/brigade22/spring/springlabs/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,9 @@
 package com.brigade22.spring.springlabs.exceptions;
 
-//@ResponseStatus(code=HttpStatus.NOT_FOUND)
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code= HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException() {
         super();
