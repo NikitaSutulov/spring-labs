d51bca4aa6329a5df44a2445730528e0f9c2badf
svitbar <svitbar1102@gmail.com>
2023-12-20 22:15:10 +0200

    Refactor Translation and Word entities

--- src/main/java/com/brigade22/spring/springlabs/entities/Translation.java
+++ src/main/java/com/brigade22/spring/springlabs/entities/Translation.java
@@ -1,13 +1,41 @@
 package com.brigade22.spring.springlabs.entities;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Entity
 public class Translation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne
     private Dictionary dictionary;
+
+    @Setter
     @NotNull
+    @ManyToOne
     private Word word;
+
+    @Setter
     @NotNull
+    @ManyToOne
     private Word translatedWord;
+
+    public Translation() {
+    }
+
+    public Translation(Long id, Dictionary dictionary, Word word, Word translatedWord) {
+        this.id = id;
+        this.dictionary = dictionary;
+        this.word = word;
+        this.translatedWord = translatedWord;
+    }
 
     public Translation(Dictionary dictionary, Word word, Word translatedWord) {
         this.dictionary = dictionary;
@@ -18,28 +46,5 @@ public class Translation {
     public Translation(Word word, Word translatedWord) {
         this.word = word;
         this.translatedWord = translatedWord;
-    }
-
-    public Dictionary getDictionary() {
-        return dictionary;
-    }
-
-    public Word getWord() {
-        return word;
-    }
-
-    public void setWord(Word word) {
-        this.word = word;
-    }
-
-    public Word getTranslatedWord() {
-        return translatedWord;
-    }
-
-    public void setTranslatedWord(Word translatedWord) {
-        this.translatedWord = translatedWord;
-    }
-    public void setDictionary(Dictionary dictionary) {
-        this.dictionary = dictionary;
     }
 }
--- src/main/java/com/brigade22/spring/springlabs/entities/Word.java
+++ src/main/java/com/brigade22/spring/springlabs/entities/Word.java
@@ -1,30 +1,38 @@
 package com.brigade22.spring.springlabs.entities;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Getter
     @NotBlank
     private String value;
-    private final Language language;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    private Language language;
+
+    @OneToMany(mappedBy = "word")
     private final List<Translation> translations = new ArrayList<>();
+
+    public Word() {
+    }
 
     public Word(Language language, String value) {
         this.language = language;
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public Language getLanguage() {
-        return language;
-    }
-
-    public void setValue(String value) {
         this.value = value;
     }
 }
