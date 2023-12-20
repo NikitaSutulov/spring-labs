a8a379264abe8a77de559d257620cf68bb2605c4
Nikita Sutulov <notsid212@gmail.com>
2023-12-20 14:05:29 +0200

    refactored DictionaryRepository to extend CrudRepository for lab6

--- src/main/java/com/brigade22/spring/springlabs/repositories/DictionaryRepository.java
+++ src/main/java/com/brigade22/spring/springlabs/repositories/DictionaryRepository.java
@@ -1,22 +1,11 @@
 package com.brigade22.spring.springlabs.repositories;
 
 import com.brigade22.spring.springlabs.entities.Dictionary;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface DictionaryRepository {
-
-    Dictionary save(Dictionary dictionary);
-
-    List<Dictionary> findAll();
-
-    void delete(Dictionary dictionary);
-    Dictionary deleteById(long id);
-
-    Dictionary findById(Long id);
-
-    void clear();
-    Dictionary update(Long id, Dictionary dictionary);
+public interface DictionaryRepository  extends CrudRepository<Dictionary, Long> {
+    Dictionary findDictionaryById(Long id);
+    void deleteDictionaryById(Long id);
 }
