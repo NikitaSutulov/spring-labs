974ff0d2d284d99db001bd302cc454b35d0376f2
sviatoslav-sh-halo-lab <sviatoslav.sh@halo-lab.team>
2023-12-21 02:32:45 +0200

    updated some methods of language service to be compatible with new version of language repository

--- src/main/java/com/brigade22/spring/springlabs/services/LanguageService.java
+++ src/main/java/com/brigade22/spring/springlabs/services/LanguageService.java
@@ -2,8 +2,10 @@ package com.brigade22.spring.springlabs.services;
 
 import com.brigade22.spring.springlabs.controllers.requests.DictionaryRequest;
 import com.brigade22.spring.springlabs.entities.Language;
+import com.brigade22.spring.springlabs.exceptions.ResourceNotFoundException;
 import com.brigade22.spring.springlabs.repositories.LanguageRepository;
 import jakarta.annotation.PostConstruct;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -18,6 +20,7 @@ public class LanguageService {
         this.languageRepository = languageRepository;
     }
 
+    @Transactional
     public Language saveLanguage (Language language) {
         language.setCode(language.getCode().toLowerCase());
         return languageRepository.save(language);
@@ -27,8 +30,9 @@ public class LanguageService {
         return languageRepository.findAll();
     }
 
-    public Language deleteByCode(String code) {
-        return languageRepository.deleteByCode(code);
+    @Transactional
+    public void deleteByCode(String code) {
+        languageRepository.deleteLanguageByCode(code);
     }
 
     public Language findByCode(String code) {
@@ -36,7 +40,7 @@ public class LanguageService {
     }
 
     public Language findByName(String name) {
-        return languageRepository.findByName(name);
+        return languageRepository.findLanguageByName(name);
     }
 
     public Language updateLanguage(String code, Language language) {
@@ -51,50 +55,21 @@ public class LanguageService {
         return languageRepository.save(language);
     }
 
-    public void checkIfLanguageExists(DictionaryRequest requestDictionary) {
-        Language possibleLanguage1 = findByName(requestDictionary.getLanguage1().getName());
-
-        if (possibleLanguage1 != null && !Objects.equals(possibleLanguage1.getCode(), requestDictionary.getLanguage1().getCode())) {
-            throw new ResponseStatusException(
-                    org.springframework.http.HttpStatus.BAD_REQUEST,
-                    "Language code is wrong"
-            );
-        }
-
-        Language possibleLanguage2 = findByName(requestDictionary.getLanguage2().getName());
-
-        if (possibleLanguage2 != null && !Objects.equals(possibleLanguage2.getCode(), requestDictionary.getLanguage2().getCode())) {
-            throw new ResponseStatusException(
-                    org.springframework.http.HttpStatus.BAD_REQUEST,
-                    "Language code is wrong"
-            );
-        }
-
-        if (findByCode(requestDictionary.getLanguage1().getCode()) == null) {
-            saveLanguage(requestDictionary.getLanguage1());
-        }
+    public void checkIfLanguagesExist(DictionaryRequest requestDictionary) {
         Language language1 = findByCode(requestDictionary.getLanguage1().getCode());
-        if (findByCode(requestDictionary.getLanguage2().getCode()) == null) {
-            saveLanguage(requestDictionary.getLanguage2());
-        }
         Language language2 = findByCode(requestDictionary.getLanguage2().getCode());
 
-        if (!Objects.equals(requestDictionary.getLanguage1().getName(), language1.getName()) || !Objects.equals(requestDictionary.getLanguage2().getName(), language2.getName())) {
-            throw new ResponseStatusException(
-                    org.springframework.http.HttpStatus.BAD_REQUEST,
-                    "Language name is wrong"
+        if (language1 == null || language2 == null) {
+            throw new ResourceNotFoundException(
+                    "Language not found"
             );
         }
-    }
 
-    @PostConstruct
-    public void initializeSampleData() {
-        Language language1 = new Language("en", "English");
-        Language language2 = new Language("ua", "Ukrainian");
-        Language language3 = new Language("pl", "Polish");
-
-        languageRepository.save(language1);
-        languageRepository.save(language2);
-        languageRepository.save(language3);
+        if (!Objects.equals(language1.getName(), requestDictionary.getLanguage1().getName())
+                || !Objects.equals(language2.getName(), requestDictionary.getLanguage2().getName())) {
+            throw new ResourceNotFoundException(
+                    "Language with such name and code not found"
+            );
+        }
     }
 }
