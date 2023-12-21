33fe06ad685366f061b98b915575695e69f180f7
Oleksandr <sa44ko@gmail.com>
2023-12-21 11:48:33 +0200

    Refactored requests to use lombok.

--- src/main/java/com/brigade22/spring/springlabs/controllers/requests/DictionaryRequest.java
+++ src/main/java/com/brigade22/spring/springlabs/controllers/requests/DictionaryRequest.java
@@ -3,7 +3,11 @@ package com.brigade22.spring.springlabs.controllers.requests;
 import com.brigade22.spring.springlabs.entities.Language;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class DictionaryRequest {
 
     @NotBlank
@@ -15,27 +19,4 @@ public class DictionaryRequest {
     @Valid
     private Language language2;
 
-    public Language getLanguage1() {
-        return language1;
-    }
-
-    public Language getLanguage2() {
-        return language2;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setLanguage1(Language language1) {
-        this.language1 = language1;
-    }
-
-    public void setLanguage2(Language language2) {
-        this.language2 = language2;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/com/brigade22/spring/springlabs/controllers/requests/TranslationRequest.java
+++ src/main/java/com/brigade22/spring/springlabs/controllers/requests/TranslationRequest.java
@@ -1,27 +1,15 @@
 package com.brigade22.spring.springlabs.controllers.requests;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TranslationRequest {
     @NotBlank
     private String word;
     @NotBlank
     private String translatedWord;
-
-    public void setWord(String word) {
-        this.word = word;
-    }
-
-    public void setTranslatedWord(String translatedWord) {
-        this.translatedWord = translatedWord;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
-    public String getTranslatedWord() {
-        return translatedWord;
-    }
 
 }
